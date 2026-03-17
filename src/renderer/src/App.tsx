71f68488d668cc357f36fd6bd88182657c709f3e
Soniccooles <andrey.sonic@mail.ru>
2026-03-17 16:35:13 +0500

    Added "+" button to the terminal page. It returns to hosts tab.

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -86,6 +86,7 @@ const App = () => {
                   activeSessionId={activeSessionId}
                   onSelect={setActiveSession}
                   onClose={removeSession}
+                  onAdd={setActiveTab}
                 />
                 <TerminalPage
                   connection={session.connection}
--- src/renderer/src/components/ui/TabsList.tsx
+++ src/renderer/src/components/ui/TabsList.tsx
@@ -1,38 +1,49 @@
-import {TerminalSession} from "@/store/useStore";
+import {TabType, TerminalSession} from "@/store/useStore";
 import {Button} from "@/components/ui/button";
-import Plus from "lucide-react"
-import {Host} from "../../../../shared/types";
 
 interface TabsListProps {
   sessions: TerminalSession[]
   activeSessionId: string | null
   onSelect: (sessionId: string) => void
   onClose: (sessionId: string) => void
+  onAdd: (tab: TabType) => void
 }
 
 export const TabsList = ({sessions, activeSessionId, onSelect, onClose, onAdd}: TabsListProps) => {
   return (
-    <div className="flex flex-row bg-secondary border-b border-border overflow-x-auto">
+    <div className="flex flex-row py-1 bg-secondary border-b border-border overflow-x-auto items-center">
       {sessions.map((s) => (
         <div key={s.id} onClick={() => onSelect(s.id)}
-             className={`px-4 py-1 cursor-pointer border-b flex items-center gap-2 min-w-[150px]
+             className={`px-4 cursor-pointer border-b flex gap-1 min-w-[100px]
                     ${activeSessionId === s.id ? 'bg-background border-b border-primary' : 'text-foreground hover:bg-background'}`}
         >
 
-        <span className="truncate text-xs">{s.connection.name || 'Session'}  </span>
-
         <Button
-          className="ml-auto h-5.5 w-5.5 hover:text-destructive cursor-pointer"
+          className="ml-auto h-5 w-5 hover:text-destructive cursor-pointer"
           variant="ghost"
           size="icon-sm"
           onClick={(e) => {
             e.stopPropagation();
             onClose(s.id);
           }}
-          >
+        >
+          ×
         </Button>
+
+        <span className="truncate text-xs">{s.connection.name || 'Session'}  </span>
         </div>
       ))}
+      <Button
+        className="ml-2 shrink-0 h-5 w-5 hover:text-success cursor-pointer"
+        variant="ghost"
+        size="icon-sm"
+        onClick={(e) => {
+          e.stopPropagation();
+          onAdd('hosts')
+        }}
+      >
+        +
+      </Button>
     </div>
   )
 }
